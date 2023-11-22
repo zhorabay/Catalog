8675e70e5a89efa7509972e7bc56ddb6d3c0553d
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-11-22 18:57:05 +0600

    complete the game class logic and author logic

---
+++ classes/game.rb
@@ -0,0 +1,16 @@
+require './item'
+require 'date'
+
+class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    super && (Date.today.year - @last_played_at.year) > 2
+  end
+end
